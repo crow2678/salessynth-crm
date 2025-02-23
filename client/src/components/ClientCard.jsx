a59988402a2dd2139e69e6988ad5785fe6167b5b
crow2678 <64113416+crow2678@users.noreply.github.com>
2025-02-23 08:17:25 -0800

    intelligence summary

--- client/src/components/ClientCard.jsx
+++ client/src/components/ClientCard.jsx
@@ -15,7 +15,7 @@ import {
   Snowflake,
   TrendingDown,
   Star,
-  atom  // Added for intelligence feature
+  Atom  // Added for intelligence feature
 } from 'lucide-react';
 import { STATUS_CONFIG, getClientStatus, getDealStatus } from '../utils/statusUtils';
 
